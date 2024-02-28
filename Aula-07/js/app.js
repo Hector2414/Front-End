32227a9f9947041e69e8c2df65b4c1b55fa15842
Hector2414 <hectorlaucher6@gmail.com>
2024-02-28 11:51:52 -0300

    trabalhando com mais um pouco de javaScript

--- Aula-07/js/app.js
+++ Aula-07/js/app.js
@@ -1,32 +1,50 @@
 //capturar os eventos do formulário
 const formulario = document.querySelector('#formulario')
 
-formulario.addEventListener('submit', function(e){ // a função aguarda o usuario clicar no botão
-e.preventDefault(); //muda o comportamento padrão do formulario que estammos escutando
+formulario.addEventListener('submit', function (e) { // a função aguarda o usuario clicar no botão
+  e.preventDefault(); //muda o comportamento padrão do formulario que estammos escutando
 
-    const inputPeso = e.target.querySelector('#peso')
-     const inputAltura = e.target.querySelector('#altura')
-    
-    
-     const peso = Number(inputPeso.value)
-   const altura = Number(inputAltura.value)
-
-   const imc = calcularIMC(peso, altura)
-   
+  const inputPeso = e.target.querySelector('#peso')
+  const inputAltura = e.target.querySelector('#altura')
 
 
-   alert("seu IMC é: " + imc)
-   //console.log(imc)
-    
-   
- /*  
-   if(imc > 30){
-alert('você está obeso')
-}else if(imc => 18 || imc  24)
+  const peso = Number(inputPeso.value)
+  const altura = Number(inputAltura.value)
+
+
+  const imc = calcularIMC(peso, altura)
+  const classificacao = tabelaImc(imc)
+  console.log(classificacao)
+  mostrarImc (classificacao)
+
+
 })
-*/
+
 //função  para calcular o IMC e retornar o valor do IMC com duas casas decimais
-function calcularIMC(peso,altura){
-    let imc = (peso / altura **2)//formula do IMC
-    return imc.toFixed(2); // formata  o número em duas casa decimais
+function calcularIMC(peso, altura) {
+  let imc = (peso / altura ** 2)//formula do IMC
+  return imc.toFixed(2); // formata  o número em duas casa decimais
 }
+
+//criar função para mostrar o IMC na tela 
+function mostrarImc(msg) {
+  const r = document.querySelector("#a");
+  console.log(r)
+  r.innerHTML = msg;
+}
+function tabelaImc(imc) {
+  const classificacao = ['abaixo do peso', 'peso normal', 'sobrepeso', 'obesidade grau I', 'obesidade grau II', 'obesidade grau III'];
+
+  if (imc >= 39.9) { return classificacao[5] }
+  if (imc >= 34.9) { return classificacao[4] }
+  if (imc >= 29.9) { return classificacao[3] }
+  if (imc >= 24.9) { return classificacao[2] }
+  if (imc >= 18.5) { return classificacao[1] }
+  if (imc < 18.5) { return classificacao[0] }
+
+}
+
+
+
+
+
